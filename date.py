7636c7ed65ee4d4bc476d047a9a0930d19dcafb7
Mohamed Coulibali <konoufo@hotmail.fr>
2020-12-27 07:09:17 -0500

    FCT: ajoute support pour des durées en heures, minutes, secondes

--- date.py
+++ date.py
@@ -5,14 +5,67 @@ class Date:
 
     def __init__(self, date=None):
         self.annee = date.annee if date is not None else 2020
-        self.mois = date.mois if date is not None else 11
-        self.jour = date.jour if date is not None else 23
-        self.heure = date.heure if date is not None else 19
-        self.minute = date.minute if date is not None else 30
-        self.seconde = date.seconde if date is not None else 00
+        self.mois = date.mois if date is not None else 11  # 0=Janvier,...,11=Décembre
+        self.jour = date.jour if date is not None else 23  # 0..31
+        self.heure = date.heure if date is not None else 19  # 0..23
+        self.minute = date.minute if date is not None else 30  # 0..59
+        self.seconde = date.seconde if date is not None else 00  # 0..59
 
-    def plus(self, jours):
+    def plus(self, jours=0, heures=0, minutes=0, secondes=0):
         """Ajoute une durée à la date courante et retourne une nouvelle date résultante"""
+        date = Date(self)
+        jours, heures, minutes, secondes = int(jours), int(heures), int(minutes), int(secondes)
+        if jours == 0 and heures == 0 and minutes == 0 and secondes == 0:
+            return date
+        jours, heures, minutes, secondes = Date.simplifieDuree(jours, heures, minutes, secondes)
+        return date.plusSecondes(secondes).plusMinutes(minutes).plusHeures(heures).plusJours(jours)
+    
+    def plusSecondes(self, secondes):
+        date = Date(self)
+        secRestantsMinute = 1
+        while secondes > 0:
+            secRestantsMinute = 59 - date.seconde
+            if secRestantsMinute < secondes:
+                date.minute = (date.minute + 1) % 60
+                date.seconde = 0
+                if date.minute == 0:
+                    date.heure = (date.heure + 1) % 24
+                    if date.heure == 0:
+                        date = date.plusJours(1)
+            else:
+                date.seconde += secondes
+            secondes -= secRestantsMinute + 1
+        return date
+
+    def plusMinutes(self, minutes):
+        date = Date(self)
+        minsRestantsHeures = 1
+        while minutes > 0:
+            minsRestantsHeures = 59 - date.minute
+            if minsRestantsHeures < minutes:
+                date.heure = (date.heure + 1) % 24
+                date.minute = 0
+                if date.heure == 0:
+                    date = date.plusJours(1)
+            else:
+                date.minute += minutes
+            minutes -= minsRestantsHeures + 1
+        return date
+
+    def plusHeures(self, heures):
+        date = Date(self)
+        heuresRestantsJours = 1
+        while heures > 0:
+            heuresRestantsJours = 23 - date.heure
+            if heuresRestantsJours < heures:
+                date.heure = 0
+                date = date.plusJours(1)
+            else:
+                date.heure += heures
+            heures -= heuresRestantsJours + 1
+        return date
+
+    def plusJours(self, jours):
         date = Date(self)
         joursRestantsMois = 1
         while jours > 0:
@@ -26,6 +79,7 @@ class Date:
                 date.mois = (date.mois + 1) % 12
                 date.jour = 1
                 if date.mois == 0:
+                    # si on passe à Janvier, alors on incrémente l'année
                     date.annee += 1
             else:
                 # sinon on ajoute juste les jours
@@ -33,6 +87,17 @@ class Date:
             jours -= joursRestantsMois + 1
         return date
     
+    @staticmethod
+    def simplifieDuree(jours=0, heures=0, minutes=0, secondes=0):
+        """Retourne une durée simplifiée dans les bornes de chaque unité de temps"""
+        minutes += secondes // 60
+        secondes = secondes % 60
+        heures += minutes // 60
+        minutes = minutes % 60
+        jours += heures // 24
+        heures = heures % 24
+        return jours, heures, minutes, secondes
+
     def estBissextile(self):
         """Retourne si cette date est dans une année bissextile"""
         return abs(self.annee - 2020) % 4 > 0  # puisque 2020 est bissextile
--- tests.py
+++ tests.py
@@ -30,7 +30,81 @@ class DateTestCase(unittest.TestCase):
         assert date1.annee == date0.annee + 1
         assert date1.mois == 0
         assert date1.jour == 1
+    
+    def test_ajoutDHeuresSansChangerDeJour(self):
+        date0 = date.Date()  # 23 décembre 2020 19:30:00
 
+        date1 = date0.plus(heures=2)
+
+        assert date1.annee == date0.annee
+        assert date1.mois == date0.mois
+        assert date1.jour == date0.jour
+        assert date1.heure == 21
+        assert date1.minute == date0.minute
+
+    def test_ajoutDHeuresQuiChangentDeJour(self):
+        date0 = date.Date()  # 23 décembre 2020 19:30:00
+
+        date1 = date0.plus(heures=5)
+
+        assert date1.annee == date0.annee
+        assert date1.mois == date0.mois
+        assert date1.jour == 24
+        assert date1.heure == 0
+        assert date1.minute == date0.minute
+
+    def test_ajoutDHeuresQuiChangentDeMois(self):
+        date0 = date.Date()
+        date0.mois = 8
+        date0.jour = 28  # 28 septembre 2020 19:30:00
+
+        date1 = date0.plus(heures=72)
+
+        assert date1.annee == date0.annee
+        assert date1.mois == 9
+        assert date1.jour == 1
+        assert date1.heure == date0.heure
+        assert date1.minute == date0.minute
+
+    def test_ajoutMinutes(self):
+        date0 = date.Date()  # 23 décembre 2020 19:30:00
+
+        date1 = date0.plus(minutes=270)  # 4h30
+
+        assert date1.annee == 2020
+        assert date1.mois == 11
+        assert date1.jour == 24
+        assert date1.heure == 0
+        assert date1.minute == 0
+
+    def test_ajoutSecondes(self):
+        date0 = date.Date()  # 23 décembre 2020 19:30:00
+
+        date1 = date0.plus(secondes=15)
+
+        assert date1.annee == date0.annee
+        assert date1.mois == date0.mois
+        assert date1.jour == date0.jour
+        assert date1.heure == date0.heure
+        assert date1.minute == date0.minute
+        assert date1.seconde == 15
+    
+    def test_ajoutDureeCombinee(self):
+        date0 = date.Date()  # 23 décembre 2020 19:30:00
+
+        date1 = date0.plus(
+            jours=21,
+            heures=2,
+            minutes=20,
+            secondes=15
+        )
+
+        assert date1.annee == 2021
+        assert date1.mois == 0
+        assert date1.jour == 13
+        assert date1.heure == 21
+        assert date1.minute == 50
+        assert date1.seconde == 15
 
 if __name__ == '__main__':
     unittest.main()
